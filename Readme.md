9261e90f96a7cb6d526831e4a694fa5d5c15dbf1
vandan.p <vandan.p@endurance.com>
2018-02-23 19:52:28 +0530

    Initial Commit with Linear Regression

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Implementation of ML Algorithms in Python
+
+Heavily inspired by Stanford's Introduction to Machine Learning Course by Andrew NG
+
+## Linear Regression
+
+### Gradient Descent
+
+### Normal Equation
---
+++ lin_reg.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+X = np.round(np.random.rand(100, 3), 3)
+Y = 3 + X[:, 0] + 5*X[:, 1] + 3*X[:, 2]
+
+def add_ones_col(mat):
+    n,m = mat.shape
+    one = np.ones((n,1))
+    return np.hstack((one, mat))
+
+def solve_normal_eqn(X, y):
+    Xnew = add_ones_col(X)
+    return np.matmul(np.linalg.inv(np.matmul(Xnew.transpose(), Xnew)),
+                     np.matmul(Xnew.transpose(), y))
+
+def compute_cost(X, y, theta):
+    n,m = X.shape
+    h = np.round(np.matmul(X, theta), 3)
+    sq_errors = np.square(np.subtract(h, y))
+    return (1.0/(2.0*m))*(sq_errors.sum())
+
+def solve_gradient_descent(X, y, alpha=0.01, n_iters=5000):
+    X = add_ones_col(X)
+    m,n = X.shape
+    theta = np.random.rand(n, 1)
+    epsilon = 100
+    for i in range(1, n_iters):
+        cost_old = compute_cost(X, Y, theta)
+        h = np.matmul(X, theta)
+        h_y = np.subtract(h, Y.reshape((100, 1)))
+        h_y_x = (np.matmul(X.transpose(), h_y)).transpose()
+        theta_new = np.subtract(theta, alpha*h_y_x.transpose())
+        cost_new = compute_cost(X, Y, theta_new)
+        epsilon = cost_old - cost_new
+        theta = theta_new
+    return theta
+
+solve_gradient_descent(X, Y)
